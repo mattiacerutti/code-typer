ee67a2e67c5df9cf828339cccf1d6703ee63d054
Clucch <mattiacerutti04@gmail.com>
2024-08-05 17:55:41 +0200

    add basic turborepo

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import {defineConfig} from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      "@lib": path.resolve(__dirname, '../../lib'),
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "../../lib"),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {target: "http://localhost:3000", changeOrigin: true},
+    },
+  },
 });
