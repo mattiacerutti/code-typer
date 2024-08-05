88041290d188c0818c1460648063771044c76bb2
Clucch <mattiacerutti04@gmail.com>
2024-08-05 15:24:21 +0200

    perf: Add GitHub API caching and significantly reduce the number of API calls

--- apps/backend/src/modules/snippets/services/snippet-fetch.service.ts
+++ apps/backend/src/modules/snippets/services/snippet-fetch.service.ts
@@ -4,58 +4,54 @@ import { lastValueFrom } from 'rxjs';
 import { LanguageName } from '@lib/types/CodeLanguage';
 import { getSupportedLanguage } from '@lib/utils/game-utils';
 import 'dotenv/config';
+import { setupCache } from 'axios-cache-interceptor';
+import { getSnippetRawLink } from '@/common/utils/snippets/snippet-utils';
 
 const GITHUB_API_URL = 'https://api.github.com';
 const TOKEN = process.env.GITHUB_API_TOKEN;
 
 @Injectable()
 export class SnippetFetchService {
+  private cachedAxios = setupCache(this.http.axiosRef, {
+    interpretHeader: false,
+  });
+
   constructor(private http: HttpService) {}
   async fetchRandomCodeFiles(language: LanguageName): Promise<string[]> {
     const extensionsFilter = getSupportedLanguage(language)
       .extensions.map((extension) => `extension:${extension}`)
       .join(' ');
 
-    const request = this.http.get(`${GITHUB_API_URL}/search/code`, {
-      params: {
-        q: `language:${language} size:>3000 ${extensionsFilter}`,
-        page: Math.ceil(Math.random() * 10),
-        per_page: 100,
-      },
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-    });
+    const request = () =>
+      this.cachedAxios.get(`${GITHUB_API_URL}/search/code`, {
+        params: {
+          q: `language:${language} size:>3000 ${extensionsFilter}`,
+          page: Math.ceil(Math.random() * 10),
+          per_page: 100,
+        },
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+        cache: {
+          ttl: 1000 * 60 * 3, // 3 minutes. We can have maximum n_of_languages * 10 requests every 3 minutes. Considering 10 languages, that makes maximum 2000 requests per hour, which is well below the limit of 5000 requests per hour
+        },
+      });
 
-    let response = await lastValueFrom(request);
+    let response = await request();
 
     // Sometimes the request is succesfull but the items array is empty. In this case, we just need to re-try the request
     if (response.data.items.length === 0) {
-      response = await lastValueFrom(request);
+      response = await request();
     }
 
     if (response.data.items.length === 0) {
       throw 'Request was successfull but the items array was empty for two times in a row';
     }
 
-    return response.data.items.map((item: any) => item.url);
+    return response.data.items.map((item: any) => getSnippetRawLink(item));
   }
 
-  async fetchFileDetails(fileUrl: string): Promise<any> {
-    const request = this.http.get(fileUrl, {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-    });
-
-    const response = await lastValueFrom(request);
-
-    return response.data;
-  }
-
-  async getFileContent(fileDetails: any): Promise<string> {
-    const rawUrl = fileDetails.download_url;
-
+  async getFileContent(rawUrl: any): Promise<string> {
     const request = this.http.get(rawUrl);
 
     const response = await lastValueFrom(request);
