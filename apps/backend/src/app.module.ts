112e56feda4fb2322ebe317817fa7604459c9e50
Clucch <mattiacerutti04@gmail.com>
2024-08-05 18:25:51 +0200

    finalize turborepo config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SnippetsModule } from '@/modules/snippets/snippets.module';
 import { HttpModule } from '@nestjs/axios';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [SnippetsModule, HttpModule],
+  imports: [
+    SnippetsModule,
+    HttpModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'frontend'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
