6a66e1a277ac762b5a45943a92cae3450b9f4769
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-03-31 19:43:36 +0200

    feat: add new line indicator

--- src/components/typing-area/character.tsx
+++ src/components/typing-area/character.tsx
@@ -3,10 +3,11 @@ import {ICharacter, CharacterState, WhitespaceTypes} from "@/types/character";
 interface ICharacterProps {
   char: ICharacter;
   charHighlighting: string;
+  isSelected?: boolean;
 }
 
 function Character(props: ICharacterProps & {ref: React.RefObject<HTMLSpanElement>}) {
-  const {char, charHighlighting, ref} = props;
+  const {char, charHighlighting, ref, isSelected = false} = props;
 
   let elementClasses = "character-default";
   let elementText = char.value;
@@ -19,8 +20,16 @@ function Character(props: ICharacterProps & {ref: React.RefObject<HTMLSpanElemen
     elementClasses = "character-wrong";
   }
 
-  if (char.value == WhitespaceTypes.Space || char.value == WhitespaceTypes.NewLine) {
+  if (char.value == WhitespaceTypes.Space) {
     elementText = "\u00A0";
+  }
+
+  if (char.value == WhitespaceTypes.NewLine) {
+    if(isSelected) {
+      elementText = "\u00A0\u00A0⮐";
+    } else if(char.state == CharacterState.Wrong) {
+      elementText = "\u00A0\u00A0⮐";
+    }
   }
 
   if (char.value == WhitespaceTypes.Tab) {
@@ -32,10 +41,7 @@ function Character(props: ICharacterProps & {ref: React.RefObject<HTMLSpanElemen
   }
 
   return (
-    <span
-      className={elementClasses}
-      ref={ref}
-    >
+    <span className={`${elementClasses} flex items-center justify-center`} ref={ref} style={{fontSize: char.value == WhitespaceTypes.NewLine ? "1rem" : "", fontWeight: char.value == WhitespaceTypes.NewLine ? "bold" : ""}}>
       {elementText}
     </span>
   );
