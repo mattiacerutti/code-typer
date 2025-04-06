f87511c85836d5de60ff6c5ab5b048953f597c15
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-04-06 18:39:56 +0200

    fix: fix minor issues

--- src/lib/client/game-logic.ts
+++ src/lib/client/game-logic.ts
@@ -1,12 +1,10 @@
-import {CharacterState, CharacterTypes, WhitespaceTypes} from "@/types/character";
+import {CharacterState, CharacterTypes} from "@/types/character";
 import { IParsedSnippet } from "@/types/snippet";
 
 export function isGameFinished(parsedSnippets: IParsedSnippet) {
   const allCorrect = parsedSnippets.every(
     (char) =>
       char.state === CharacterState.Right ||
-      char.value === WhitespaceTypes.Tab ||
-      (char.value === WhitespaceTypes.NewLine && char.state === CharacterState.Default) ||
       char.type === CharacterTypes.EOF
   );
 
