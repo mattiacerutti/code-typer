0c22d9900121333ed6a0e7e2e900a4e2cae7c689
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-04-09 18:11:25 +0200

    feat: add internal API header check and enhanced rate limiter

--- src/services/client/snippets/snippet-fetch.service.ts
+++ src/services/client/snippets/snippet-fetch.service.ts
@@ -1,9 +1,16 @@
 import { ILanguage } from "@/types/language";
 import { ISnippet } from "@/types/server/snippet";
+
+const internalApiHeader = {
+  "x-internal-api": "true",
+}
+
 export async function fetchRandomSnippets(languageId: string): Promise<ISnippet[]> {
   const url = `api/snippets/random?language=${encodeURIComponent(languageId)}`;
 
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: internalApiHeader,
+  });
 
   if (!response.ok) {
     throw new Error(`Request failed with status code ${response.status}`);
@@ -15,7 +22,9 @@ export async function fetchRandomSnippets(languageId: string): Promise<ISnippet[
 export async function fetchLanguages(): Promise<{[key: string]: ILanguage}> {
   const url = `api/snippets/languages`;
 
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: internalApiHeader,
+  });
 
   if (!response.ok) {
     throw new Error(`Request failed with status code ${response.status}`);
