584c3f6982a6ee33effb33fa4c983b5c2a4e0e92
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-10-06 19:02:08 +0200

    fix: fix small issue with autoclosing chars when typing

--- src/features/game/logic/typing/add-character.ts
+++ src/features/game/logic/typing/add-character.ts
@@ -2,14 +2,28 @@ import {getChar, getPreviousChar, hasOnlyWhitespacesBefore, setCharacterState} f
 import {CharacterState, CharacterTypes} from "@/shared/types/character";
 import {IParsedSnippet} from "@/shared/types/snippet";
 
-function incrementUserPosition(snippet: IParsedSnippet, position: number, updateUserPosition: (position: number) => void): number {
+function incrementUserPosition(snippet: IParsedSnippet, position: number, updateUserPosition: (position: number) => void, isPressedKeyCorrect: boolean): number {
   const newChar = getChar(snippet, position + 1);
 
-  if ((newChar.type === CharacterTypes.Whitespace && hasOnlyWhitespacesBefore(snippet, position + 1)) || (newChar.type === CharacterTypes.AutoClosing && !newChar.isOpening)) {
-    return incrementUserPosition(snippet, position + 1, updateUserPosition);
+  if (
+    (newChar.type === CharacterTypes.Whitespace && hasOnlyWhitespacesBefore(snippet, position + 1)) ||
+    (newChar.type === CharacterTypes.AutoClosing && !newChar.isOpening && (isPressedKeyCorrect || snippet[newChar.pairedChar].state === CharacterState.Right))
+  ) {
+    return incrementUserPosition(snippet, position + 1, updateUserPosition, isPressedKeyCorrect);
   }
 
   return position + 1;
+}
+
+function getSignificantPreviousChar(snippet: IParsedSnippet, position: number): IParsedSnippet[number] | null {
+  const prevChar = getPreviousChar(snippet, position);
+  if (!prevChar) return null;
+
+  if (prevChar.type === CharacterTypes.AutoClosing && !prevChar.isOpening) {
+    return getSignificantPreviousChar(snippet, position - 1);
+  }
+
+  return prevChar;
 }
 
 export function addCharacter(
@@ -23,19 +37,21 @@ export function addCharacter(
   const expectedChar = getChar(snippet, position);
 
   if (expectedChar.value === "EOF") return;
+;
 
   if (pressedKey === "Enter") {
     pressedKey = "\n";
   }
   let isPressedKeyCorrect = pressedKey === expectedChar.value;
 
-  const newPosition = incrementUserPosition(snippet, position, updateUserPosition);
-
-  // If the previous character was incorrect, we also set this to incorrect no matter what.
-  const prevChar = getPreviousChar(snippet, position);
+    // If the previous character was incorrect, we also set this to incorrect no matter what.
+  const prevChar = getSignificantPreviousChar(snippet, position)
   if (prevChar && prevChar.state === CharacterState.Wrong) {
     isPressedKeyCorrect = false;
   }
+
+  const newPosition = incrementUserPosition(snippet, position, updateUserPosition, isPressedKeyCorrect);
+
 
   for (let i = position; i <= newPosition - 1; i++) {
     const char = snippet[i];
