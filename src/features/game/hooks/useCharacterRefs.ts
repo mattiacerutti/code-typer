485b4b13875f30cd3cd38b720498c476b923c701
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-11-10 18:11:38 +0100

    fix: remove unused hook

--- src/features/game/hooks/useCharacterRefs.ts
+++
@@ -1,7 +0,0 @@
-import {IParsedSnippet} from "@/shared/types/snippet";
-import React from "react";
-import {useRef} from "react";
-
-export const useCharacterRefs = (snippet: IParsedSnippet) => {
-  return useRef(snippet.map(() => React.createRef<HTMLSpanElement>()));
-};
