d17a7d0f00a80592ed27b3c90050a99a3cf2b44a
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-11-11 14:50:48 +0100

    fix: focus input when changing snippet

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -118,6 +118,7 @@ function Home() {
     setStatus(GameStatus.LOADING);
     await goToNextSnippetWithPrefetch();
     resetStopwatch();
+    hiddenInputRef.current?.focus();
   };
 
   const handleChangeLanguage = async (selectedLanguage: ILanguage) => {
