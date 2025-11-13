f3d217f1119bb05c0a4754c5a965424dbf928e58
Mattia Cerutti <mattiacerutti04@gmail.com>
2025-11-13 11:13:59 +0100

    feat: change github button text

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -12,7 +12,7 @@ function GameLayout({children}: {children: React.ReactNode}) {
       <footer className="flex items-center justify-center gap-2 py-4 text-sm text-slate-500">
         <Link href="https://github.com/mattiacerutti/code-typer" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-slate-900">
           <FaGithub className="text-lg" />
-          <span>GitHub</span>
+          <span>Star it on GitHub ⭐</span>
         </Link>
       </footer>
     </div>
